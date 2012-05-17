db259bb6e5b3fd33ff12e74755d395f871b15c85
Nick Amante <namante@gmail.com>
2012-05-17 11:37:18 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NickamanteCom::Application.config.secret_token = '7a576a945fe82920e34333ab865ab64dca3611ada157ef0fe0d27ad7722e2d2672b5eb87eef95f5d7d2b94cdaa02fce627b9c93674621bff4fdc8519557517d8'
